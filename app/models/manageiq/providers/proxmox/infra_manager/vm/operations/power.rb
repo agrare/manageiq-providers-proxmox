2842fac527af88aa2c058bc1c25c837c43d0eedd
Adam Grare <adam@grare.com>
2026-01-21 10:55:28 -0500

    Add more VM power operations

--- app/models/manageiq/providers/proxmox/infra_manager/vm/operations/power.rb
+++ app/models/manageiq/providers/proxmox/infra_manager/vm/operations/power.rb
@@ -3,13 +3,38 @@ module ManageIQ::Providers::Proxmox::InfraManager::Vm::Operations::Power
 
   def raw_start
     with_provider_connection do |connection|
-      connection.request(:post, "/nodes/#{host.ems_ref}/qemu/#{ems_ref}/status/start")
+      action = raw_power_state == "paused" ? "resume" : "start"
+      connection.request(:post, "/nodes/#{host.ems_ref}/qemu/#{ems_ref}/status/#{action}")
     end
   end
 
   def raw_stop
     with_provider_connection do |connection|
       connection.request(:post, "/nodes/#{host.ems_ref}/qemu/#{ems_ref}/status/stop")
+    end
+  end
+
+  def raw_suspend
+    with_provider_connection do |connection|
+      connection.request(:post, "/nodes/#{host.ems_ref}/qemu/#{ems_ref}/status/suspend")
+    end
+  end
+
+  def raw_reboot_guest
+    with_provider_connection do |connection|
+      connection.request(:post, "/nodes/#{host.ems_ref}/qemu/#{ems_ref}/status/reboot")
+    end
+  end
+
+  def raw_reset
+    with_provider_connection do |connection|
+      connection.request(:post, "/nodes/#{host.ems_ref}/qemu/#{ems_ref}/status/reset")
+    end
+  end
+
+  def raw_shutdown_guest
+    with_provider_connection do |connection|
+      connection.request(:post, "/nodes/#{host.ems_ref}/qemu/#{ems_ref}/status/shutdown")
     end
   end
 end
