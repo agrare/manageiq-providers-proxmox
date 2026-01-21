4b969b963dc471615783b25846de75d48e50bcff
Adam Grare <adam@grare.com>
2026-01-21 09:57:56 -0500

    Calculate raw_power_state

--- app/models/manageiq/providers/proxmox/infra_manager/vm.rb
+++ app/models/manageiq/providers/proxmox/infra_manager/vm.rb
@@ -1,2 +1,12 @@
 class ManageIQ::Providers::Proxmox::InfraManager::Vm < ManageIQ::Providers::InfraManager::Vm
+  POWER_STATES = {
+    "running"   => "on",
+    "stopped"   => "off",
+    "paused"    => "paused",
+    "suspended" => "suspended"
+  }.freeze
+
+  def self.calculate_power_state(raw_power_state)
+    POWER_STATES[raw_power_state] || super
+  end
 end
